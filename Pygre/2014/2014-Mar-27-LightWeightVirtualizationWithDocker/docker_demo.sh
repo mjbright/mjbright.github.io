262d3da7caaebe7ad5abadebd8da92e5316f9209
Michael <github@mjbright.net>
2014-03-27 10:08:30 +0100

    Semi-auto push: Updated script++

--- Pygre/2014/2014-Mar-27-LightWeightVirtualizationWithDocker/docker_demo.sh
+++ Pygre/2014/2014-Mar-27-LightWeightVirtualizationWithDocker/docker_demo.sh
@@ -669,6 +669,36 @@ EOF
     SHOW_DOCKER run -p 80:80 -i -t $_USER/py_wsgi
 }
 
+DEMO10() {
+    BANNER "Inspecting images"
+
+    CNT=mysql
+
+    echo; pause "Let's look at the history of mjbright/$CNT"
+    SHOW_DOCKER history mjbright/$CNT
+
+    echo; pause "Inspecting processes $CNT"
+    SHOW_DOCKER top $CNT
+
+    echo; pause "Inspecting $CNT"
+    SHOW_DOCKER inspect $CNT
+
+    echo; pause "Inspecting $CNT => get ip address"
+    set -x
+    docker inspect $CNT | grep IPAddress | cut -d '"' -f 4
+    docker inspect $CNT | jq -r '.[0].NetworkSettings.IPAddress'
+    set +x
+
+    echo; pause "Look at all images related to mjbright/$CNT"
+    set -x
+    docker images -v mjbright/$CNT | dot -Tpng -o docker.png
+    set +x
+
+    echo "Now look at http://localhost:8080"
+    python -m SimpleHTTPServer 8080
+
+}
+
 DEMO9() {
     BANNER "Demo creation of a Python twistd image, then run as a container"
 
